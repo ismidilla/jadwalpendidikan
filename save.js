ba999146d4782e0cf9adc7af01e6c6ae58d7090e
ismidilla <kusmiatii.engkus@gmail.com>
2017-01-05 07:58:22 +0700

    Create save.js

---
+++ save.js
@@ -0,0 +1,94 @@
+<center>
+<h2 id="daplong" style="color: daplong;">
+Please Wait...</h2>
+</center>
+<center>
+Next URL</center>
+<center>
+<script async="" src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Panjang Horizontal -->
+<ins class="adsbygoogle" data-ad-client="ca-pub-2177982793611531" data-ad-slot="4023976403" style="display: inline-block; height: 90px; width: 728px;"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</center>
+<div style="background-color: white; text-align: justify;">
+<span style="color: #333333; font-family: &quot;trebuchet ms&quot; , sans-serif; font-size: 12.0pt; line-height: 115%;">You probably know that
+high-end industrial 3d printers can print metal objects by using laser or
+electron beams on fine metallic powder but Vader is something completely
+different. Vader aims at small companies or professional workshop environments
+by implementing proprietary MagnetoJet Printing (MJP) and magnetohydrodynamic
+(MHD) ceramic printhead&nbsp;technologies to reduce the machine cost.</span><span style="color: #333333; font-family: &quot;calibri&quot; , &quot;sans-serif&quot;; font-size: 11.0pt; line-height: 115%;"> </span><br />
+<center>
+<span style="color: #333333;"><span style="font-family: &quot;trebuchet ms&quot; , sans-serif;"><span style="font-family: &quot;times&quot; , &quot;times new roman&quot; , serif;">
+</span><b><span style="font-size: 12pt; line-height: 115%;"><span style="font-family: &quot;times&quot; , &quot;times new roman&quot; , serif;">
+<script async="" src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Panjang Horizontal -->
+<ins class="adsbygoogle" data-ad-client="ca-pub-2177982793611531" data-ad-slot="4023976403" style="display: inline-block; height: 90px; width: 728px;"></ins></span>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</span></b></span></span></center>
+<span style="color: #333333;"><span style="font-family: &quot;trebuchet ms&quot; , sans-serif;">
+<b style="font-family: 'trebuchet ms', sans-serif;"><span style="font-family: &quot;cambria&quot; , serif; font-size: 12pt; line-height: 115%;"> </span></b><br />
+</span></span><br />
+<center style="font-family: 'trebuchet ms', sans-serif;">
+<span style="color: #333333;"><span style="font-family: &quot;trebuchet ms&quot; , sans-serif;">
+<b><span style="font-family: &quot;cambria&quot; , serif; font-size: 12pt; line-height: 115%;">
+Skip 3D Printing</span></b></span></span></center>
+<div style="background-position: initial initial; background-repeat: initial initial;">
+<div style="background-position: initial initial; background-repeat: initial initial;">
+<span style="color: #333333; font-family: &quot;trebuchet ms&quot; , sans-serif;">It
+uses molten metal and jets it in small droplets onto a surface similary to a
+standard&nbsp;inkjet paper printer and by connecting the metal droplets a 3d
+object can be made. It could be considered world’s first molten metal 3d
+printer!<o:p></o:p></span></div>
+<div style="background-position: initial initial; background-repeat: initial initial;">
+<span style="color: #333333; font-family: &quot;trebuchet ms&quot; , sans-serif;"><br /></span></div>
+</div>
+<div style="background-position: initial initial; background-repeat: initial initial;">
+<div style="background-position: initial initial; background-repeat: initial initial;">
+<span style="color: #333333; font-family: &quot;trebuchet ms&quot; , sans-serif;">Since
+one of the design ideas behind this machine is to make it widely available, the
+material it uses had to be widely available and cheap so the main material is
+molten aluminum wire.<o:p></o:p></span></div>
+</div>
+<div style="background-position: initial initial; background-repeat: initial initial; box-sizing: border-box; margin-bottom: 1.8rem; margin-top: 1.8rem; text-align: start;">
+<div style="background-position: initial initial; background-repeat: initial initial;">
+<span style="color: #333333; font-family: &quot;trebuchet ms&quot; , sans-serif;">While
+the basic technology sounds easy in theory and has been used even 20 years ago
+in the past for&nbsp;some applications, there are still many difficult
+technical obstacles to overcome. These obstacles include reaching complex
+requirements like developing a printhead that is resistant to abrasive
+properties of molten aluminum by using ceramics, magnetic containment and
+getting drop-on-demand molten metal droplets.<o:p></o:p></span></div>
+</div>
+<div class="MsoNormal">
+<div class="MsoNormal">
+<span style="font-family: &quot;trebuchet ms&quot; , sans-serif;"><span style="color: #333333; font-size: 12pt; line-height: 115%;">Here is a detailed Vader presentation given on March 16, 2015
+at the University at Buffalo Horizons in Digital Manufacturing Conference. Very
+interesting stuff if you are interested in liquid molten metal dynamics,
+microdroplets deposition and jet formation.</span><o:p></o:p></span></div>
+<div class="MsoNormal">
+<span style="font-family: &quot;trebuchet ms&quot; , sans-serif;"><span style="color: #333333; font-size: 12pt; line-height: 115%;">Vader is founded two years ago by Scott and Zachary Vader, a
+father-son team. With Fall of 2015 being the timeframe for the release of first
+working pre-production Mark 1 prototype.</span><o:p></o:p></span></div>
+</div>
+<div class="MsoNormal">
+<span style="font-family: &quot;trebuchet ms&quot; , sans-serif;"><o:p></o:p></span></div>
+<div class="MsoNormal">
+<span style="color: #333333; font-family: &quot;trebuchet ms&quot; , sans-serif; font-size: 12.0pt; line-height: 115%;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</span><br />
+<div class="MsoNormal">
+<span style="font-family: &quot;trebuchet ms&quot; , sans-serif;"><span style="color: #333333; font-size: 12pt; line-height: 115%;">The price range of the first machine should be
+around 250.000 USD which is much cheaper than currently available commercial
+metal 3d printers.</span> Hopefully the Vader team will be successful in
+bringing their machine to the market and start the race for affordable HOME
+metal 3d printers. I want one! And I know some of you want one too!</span><o:p></o:p></div>
+</div>
+</div>
+<div style="color: #333333; font-size: 12pt; line-height: 115%; text-align: justify;">
+<span style="background-color: white; font-family: &quot;trebuchet ms&quot; , sans-serif; font-size: 12pt; line-height: 115%;"><br /></span></div>
+<div style="color: #333333; font-size: 12pt; line-height: 115%; text-align: justify;">
+<span style="background-color: white; font-family: &quot;trebuchet ms&quot; , sans-serif; font-size: 12pt; line-height: 115%;"><br /></span></div>
+<div style="text-align: justify;">
+<span style="font-family: &quot;times&quot; , &quot;times new roman&quot; , serif;"><span style="background-color: white; color: #333333; font-size: 12pt; line-height: 115%;">Vader System company page :&nbsp;</span><span style="color: #333333; font-family: &quot;trebuchet ms&quot; , sans-serif;"><span style="line-height: 18.399999618530273px;">http://www.vadersystems.com</span></span></span></div>
